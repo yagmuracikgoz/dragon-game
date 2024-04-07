65e1f754706d6bef661faeb863181795938937a9
ALPER <icmyagmuracikgoz@gmail.com>
2024-04-08 00:14:24 +0300

    some changes

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
 let xp = 0;
 let health = 100;
 let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+// adding variables to the elements
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// creating functions
+function goTown() {
+    update(locations);
+}
+function goStore() {
+    // // console.log("Going to store.");
+    // button1.innerText="Buy 10 health (10 gold)";
+    // button2.innerText = "Buy weapon (30 gold)";
+    // button3.innerText = "Go to town square";
+    // button1.onclick =buyHealth;
+    // button2.onclick=buyWeapon;
+    // button3.onclick=goTown;
+    // text.innerText="You enter the store.";
+}
+function goCave(){
+    console.log("Going to cave.");
+}
+function fightDragon(){
+    console.log("Fighting dragon.");
+}
+function buyHealth() {
+
+}
+function buyWeapon() {
+
+}
+function update(location) {
+ button1.innerText = "Go to store";
+ button2.innerText = "Go to cave";
+ button3.innerText = "Fight dragon";
+ button1.onclick = goStore;
+ button2.onclick = goCave;
+ button3.onclick = fightDragon;
+ text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
+
+
+// adding events to the buttons. when clicked to the buttons, it will call the functions.
+button1.onclick=goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+const locations= [
+    {
+     name: "town square",
+     "button text": ["Go to store", "Go to cave", "Fight dragon"],
+     "button functions": [goStore, goCave, fightDragon],
+     text: "You are in the town square. You see a sign that says \"Store\".",
+    },
+    {
+     name:"store",
+     "button text":["Buy 10 health (10 gold)","Buy weapon (30 gold)","Go to town square"],
+     "button functions":[buyHealth,buyWeapon,goTown],
+     text:"You enter the store."
+    }
+];
