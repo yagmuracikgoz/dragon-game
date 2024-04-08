a7d1d45180486fd10aa399f93bb19ee4c4292a73
ALPER <icmyagmuracikgoz@gmail.com>
2024-04-08 21:43:20 +0300

    trying different things

--- script.js
+++ script.js
@@ -107,6 +107,17 @@ function buyWeapon() {
         button2.onclick = sellWeapon;
     }
 }
+function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
+    }
+}
 
 function fightSlime() {
 
