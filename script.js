225ee1cde8bfd68d3814395c77a711cac957c6c7
ALPER <icmyagmuracikgoz@gmail.com>
2024-04-09 23:43:54 +0300

    new day

--- script.js
+++ script.js
@@ -154,7 +154,11 @@ function fightDragon() {
 }
 
 function goFight() {
-
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
