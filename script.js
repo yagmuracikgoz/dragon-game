ff545d456b37847b9d4e67c4cf357b79f6dac4be
ALPER <icmyagmuracikgoz@gmail.com>
2024-04-22 16:23:50 +0300

    step 134

--- script.js
+++ script.js
@@ -183,6 +183,8 @@ function dodge() {
 function defeatMonster(){
   gold+=Math.floor(monsters[fighting].level*6.7);
   xp+=monsters[fighting].level;
+  goldText.innerText=gold;
+  xpText.innerText=xp;
 
 
 }
