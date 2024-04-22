65aed82da82417845770270ab008cebf150c4cef
ALPER <icmyagmuracikgoz@gmail.com>
2024-04-22 16:03:58 +0300

    step 128

--- script.js
+++ script.js
@@ -169,6 +169,9 @@ function attack() {
   monsterHealth-=weapons[currentWeapon].power+ Math.floor(Math.random()* xp) + 1;
   healthText.innerText=health;
   monsterHealthText.innerText=monsterHealth;
+  if(health<=0){
+    lose();
+  }
 }
 
 function dodge() {
